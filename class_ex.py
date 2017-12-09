73f1cf5473b9d9d241998b0d2a92b7293bfa5058
vimana <vimana@nili.ca>
2017-12-08 19:03:11 -0700

    add new codes

---
+++ class_ex.py
@@ -0,0 +1,12 @@
+class Person:
+ def __init__(self, name, job):
+   self.name = name
+   self.job = job
+ def info(self):
+   return (self.name, self.job)
+
+rec1 = Person('mel', 'trainer')
+rec2 = Person('vls', 'developer')
+
+print rec1.job,rec2.info()
+
---
+++ randomWalk.py
@@ -0,0 +1,17 @@
+# Kip Thorne <Modern Classical Physics>
+# ex-6.3 random walk example
+# Shakuna Vimana
+# 2017-12-8
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+y0 = 0
+yj = []
+for i in range(1000):
+ y = random.choice([-1,1])
+ y0 = y0+y
+ yj.append(y0)
+
+hist, bin_edges = np.histogram(yj, density=True)
+plt.hist(yj,bins="auto")
+plt.show()
